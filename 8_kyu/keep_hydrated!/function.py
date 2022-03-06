f4c700e7bd2bb94a06946fd8e2a69f7c1d5d5c41
Eluent <onurbelek@outlook.com>
2022-03-06 14:56:10 +0000

    Adding the solution for Keep Hydrated!

---
+++ 8_kyu/keep_hydrated!/function.py
@@ -0,0 +1,2 @@
+def litres(time):
+    return 0
---
+++ 8_kyu/keep_hydrated!/solution.py
@@ -0,0 +1,5 @@
+import math
+def litres(time):
+    return math.floor(time*0.5)
+
+#math.floor() rounds down whereas math.ceil() rounds up to the next integer
