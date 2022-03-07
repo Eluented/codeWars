4dd2faa7dda0786db13ce072288f7acc17854586
Eluent <onurbelek@outlook.com>
2022-03-07 10:04:50 +0000

    The solution for String Repeat(too easy)

---
+++ 8_kyu/string_repeat/solution.py
@@ -0,0 +1,5 @@
+def repeat_str(repeat, string):
+    repeating_string = "" # setting things up
+    for x in range(repeat): # 0 to repeat number -1 - concatenates the repeating string repeat number of times
+        repeating_string += string 
+    return repeating_string
