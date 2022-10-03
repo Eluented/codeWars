ad58b6cf6593a06e2a26ad5934289a4bfa83d6d8
Onur <onurbelek@outlook.com>
2022-10-03 13:14:48 +0100

    added first kata solutions to java questions!

---
+++ 8_kyu/question.java
@@ -0,0 +1,6 @@
+public class Sid {
+    public static int simpleMultiplication(int n) {
+        //your code here
+        return -1;
+    }
+}
---
+++ 8_kyu/solution.java
@@ -0,0 +1,6 @@
+public class Sid {
+    public static int simpleMultiplication(int n) {
+      int mult = (n % 2 == 0) ? n * 8 : n * 9;
+      return mult;
+    }
+}
