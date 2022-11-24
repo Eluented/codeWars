6359dbcdb7783b5c6d401f573b851346b106d08b
Onur <onurbelek@outlook.com>
2022-11-24 12:53:48 +0000

    another solution

---
+++ 8_kyu/makeUpperCase/readme.md
@@ -0,0 +1,2 @@
+# MakeUpperCase
+Write a function which converts the input string to uppercase.
---
+++ 8_kyu/makeUpperCase/solution.java
@@ -0,0 +1,6 @@
+class Upper{
+  public static String MakeUpperCase(String str){
+    System.out.println(str.toUpperCase());
+    return str.toUpperCase();
+  }
+}
